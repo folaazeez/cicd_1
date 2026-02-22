d0cedcc0ec5aea4debf576d5ed5370a9242e9616
Fola Azeez <foladevops@gmail.com>
2026-02-22 09:11:37 +0000

    fix:missing cloudwatch out in foundation

--- infra/foundation/outputs.tf
+++ infra/foundation/outputs.tf
@@ -4,7 +4,7 @@ output "cluster_id" { value = aws_ecs_cluster.main.id }
 output "subnet_id" { value = aws_subnet.public_a.id }
 output "vpc_id" { value = aws_vpc.main.id }
 output "ecs_tasks_sg" { value = aws_security_group.ecs_tasks.id }
-output "aws_cloudwatch_log_group_name" {value = aws_cloudwatch_log_group.policy_gate.name}
+output "aws_cloudwatch_log_group_name" { value = aws_cloudwatch_log_group.policy_gate.name }
 output "ecs_task_execution_role_arn" {
   value = aws_iam_role.ecs_task_execution.arn
 }
