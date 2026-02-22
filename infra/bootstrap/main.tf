92d14fc9a1634c94200215c638e71d69372af4f8
Fola Azeez <foladevops@gmail.com>
2026-02-22 06:56:55 +0000

    feat:remove infra base terraform files

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "cicd_1-bucket-fola"
+  bucket = "cicd1-bucket-fola"
 }
 
 resource "aws_s3_bucket_versioning" "tf_state" {
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = "cicd_1-tbl"
+  name         = "cicd1-tbl"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
